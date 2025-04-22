1df95ade7964a14cbf2c6adbe734f7401ce7c9f5
Mina Eid <mina.eid@chefaa.com>
2025-04-22 16:42:21 +0200

    Initial Commit

---
+++ AdaptivePlayer/AdaptivePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptivePlayerApp.swift
+//  AdaptivePlayer
+//
+//  Created by OSX on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AdaptivePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
